7dd0df6a9065446fd995c1db6606546bcedddcb6
jacobsaw <adam.w.jacobs@gmail.com>
2016-11-01 10:05:45 -0400

    updated server.py

--- apps/settings.py
+++ apps/settings.py
@@ -56,13 +56,9 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'dVdDUMmbXR',
-        'HOST': 'localhost',
-        'PORT': '',
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
